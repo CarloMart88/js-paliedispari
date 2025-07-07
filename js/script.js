4057d3ee17c3b1f86bb698f55d3ff5ac1111d9ab
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-07 17:00:24 +0200

    feat: done 1 excercise

--- js/script.js
+++ js/script.js
@@ -33,20 +33,27 @@ if (parola.length >= 4 && parola.length % 2 === 0 && palindroma(parola) ) {
 }
 
 
-
-
-
-
-
-
-
-
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Consigli del giorno
 // Scriviamo sempre in italiano i passaggi che vogliamo fare
